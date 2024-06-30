45c800da36e53d41845055470b731a33fdfb4525
AlexProxoor <alex.prokhor03@gmail.com>
2024-06-30 17:10:08 +0300

    вымыыв

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Two from './components/[2]/Two';
+import Three from './components/[3]/Three';
+import TodoList from './components/[1]/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <TodoList></TodoList>
+     <Two></Two>
+     <Three></Three>
     </div>
   );
 }
+
+
+
 
 export default App;
